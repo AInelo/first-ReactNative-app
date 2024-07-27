45aaff80fb0b78e7b592a9e24dbf9f100bf16a25
TOTON Lionel <totonlionel@gmail.com>
2024-07-27 03:44:39 +0100

    My first Mobile App

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,16 +1,24 @@
 import { View, Text, ScrollView, Image } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { Link } from 'expo-router'
 
 import {images} from '../../constants'
 import FormField from '../../components/FormField'
-
+import CustomButton from '../../components/CustomButton'
 
 const SignIn = () => {
   const [form, setForm] = useState({
     email:'',
     password: ''
   })
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const submit = () => {
+
+  }
+
+
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -42,6 +50,24 @@ const SignIn = () => {
               })}
               otherStyles="mt-7"
             />
+
+            <CustomButton
+              title={`Sign In`}
+              handlePress={submit}
+              containerStyles="mt-7"
+              isLoading={isSubmitting}
+            />
+
+            <View className="justify-center pt-5 flex-row gap-2">
+                <Text className="text-lg text-gray-100 font-pregular">
+                    Don't you have an account ? 
+                </Text>
+            </View>
+              <Link href="/sign-up"
+              className='text-lg font-psemibold text-secondary text-center'
+              >
+                Sign Up
+              </Link>
         </View>
       </ScrollView>
     </SafeAreaView>
